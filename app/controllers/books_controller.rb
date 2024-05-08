b9fdd5ace804913dc4e631de0a9160ede51d2231
shinomiyasayaka <ec2-user@ip-172-31-26-76.ec2.internal>
2024-05-08 13:31:24 +0000

    3-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,49 @@
+class BooksController < ApplicationController
+  
+  def create
+     # １.&2. データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # 4. show画面へリダイレクト
+    redirect_to book_path(book.id)
+  end
+  
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  #editアクション内の@listがインスタンス変数であるのは、コントローラーからViewファイルに渡され、Viewファイルがインスタンス変数を参照する必要があるから
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+  
+  #一方updateアクションでは、リストの情報を更新しています。
+#更新後にリダイレクトするため、変数をViewファイルに渡す必要がありません。
+#このためlistという変数はupdateアクション内だけで使用するために、ローカル変数となっています。
+  
+  def destory
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
+  end
+  
+   private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  root to: "homes#top"
+
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+
+#get 'lists/:id' => 'lists#show', as: 'list'  名前付きルートとは、文字通りルーティング自体に名前をつけておくことです、
+そして名前付きルートがあると、その名前をredirect_toやlink_toでも使用することができます。
+
+
